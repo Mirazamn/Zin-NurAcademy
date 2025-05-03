abf1b43d2a512e81480a9b854f13c4dc0d9e47db
Mirazamn <mirsaidovmirazam@gmail.com>
2025-05-03 12:33:39 +0500

    AOS Animation added on branches section

--- src/components/Branches/app.jsx
+++ src/components/Branches/app.jsx
@@ -7,7 +7,7 @@ function Branches() {
     return(
         <div className="branches">
             <div className="container flex">
-                    <div className="card">
+                    <div data-aos="fade-up-right" className="card">
                         <div className="branch-txt">
                             <h2>UCHTEPA FILIALI</h2>
                             <a href='https://yandex.uz/maps/org/66856939225/?ll=69.183645%2C41.310394&z=16' target='_blank'>
@@ -17,7 +17,7 @@ function Branches() {
                         <img src={Uchtepa} className="sergeli column" />
                     </div>
 
-                    <div className="card">
+                    <div data-aos="fade-down-left" className="card">
                         <div className="branch-txt">
                             <h2>SERGELI FILIALI</h2>
                             <a href='https://yandex.uz/maps/org/zin_nur_akademiyasi/155272257265/?ll=69.210049%2C41.220986&z=16.77' target='_blank'>
